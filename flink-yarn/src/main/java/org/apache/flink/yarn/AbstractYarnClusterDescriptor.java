d4692871955b8ade429ae1600bb6b9ead59bd2de
n.fraison <n.fraison@criteo.com>
2019-06-05 17:30:00 +0200

    [FLINK-12869] Add yarn acls capability to flink containers

--- flink-yarn/src/main/java/org/apache/flink/yarn/AbstractYarnClusterDescriptor.java
+++ flink-yarn/src/main/java/org/apache/flink/yarn/AbstractYarnClusterDescriptor.java
@@ -933,6 +933,8 @@ public abstract class AbstractYarnClusterDescriptor implements ClusterDescriptor
 		amContainer.setLocalResources(localResources);
 		fs.close();
 
+		Utils.setAclsFor(amContainer, flinkConfiguration);
+
 		// Setup CLASSPATH and environment variables for ApplicationMaster
 		final Map<String, String> appMasterEnv = new HashMap<>();
 		// set user specified app master environment variables
